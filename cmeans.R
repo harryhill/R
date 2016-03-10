96e5c4145c91c3fbef2a08a17ab4424350e9a634
harryhill <eule323@gmail.com>
2016-03-10 21:13:38 +0900

    debug and add comments

--- cmeans.R
+++ cmeans.R
@@ -3,11 +3,32 @@ library(dplyr)
 library(scatterplot3d)
 library(ggplot2)
 
+#set iris$Sepal.Length, Sepal.Width, Petal.Length, Petal.Width as training data
+#    Sepal.Length Sepal.Width Petal.Length Petal.Width
+#1            5.1         3.5          1.4         0.2
+#2            4.9         3.0          1.4         0.2
+#3            4.7         3.2          1.3         0.2
+#4            4.6         3.1          1.5         0.2
+
 x <- iris[1:4]
-cm <- fanny(x,3)
+
+#use Fuzzy c means function named cluster::fanny
+cm <- cluster::fanny(x,3)
+
+#check result of FCM from membership
 cm$membership
-member <- cm$membership
+member <- as.data.frame(cm$membership)
+
+member <- member %>%
+  mutate(Species = iris$Species)
+
+#visualize result
 plot(member[,1],member[,3])
-filterm <- member %>% filter(member > 0.6) 
-scatterplot3d(member)
-scatterplot3d
+
+
+filterm <- member %>%
+  filter(V1^2+V2^2+V3 > 0.7) 
+
+#3d plot might be better for more comprehension
+scatterplot3d(filterm[,1:3])
+
